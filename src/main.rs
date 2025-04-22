36606fc925cae4596c523599a8ead4816e4baa77
AP Ljungquist <ap@ljungquist.eu>
2025-04-22 19:15:42 +0200

    f: remove acap-logging instead of records

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,11 @@
+use log::{debug, error, info, trace, warn};
+
 fn main() {
     eprintln!("Hello stderr!");
     println!("Hello stdout!");
-    acap_logging::init_logger();
+    trace!("Hello trace!");
+    debug!("Hello debug!");
+    info!("Hello info!");
+    warn!("Hello warn!");
+    error!("Hello error!");
 }
