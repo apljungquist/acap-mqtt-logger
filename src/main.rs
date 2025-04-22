73c1fe120cfeca1c6faef10e3b06bf79e9558d2f
AP Ljungquist <ap@ljungquist.eu>
2025-04-22 19:20:08 +0200

    f: no logging

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,4 @@
-use log::{debug, error, info, trace, warn};
-
 fn main() {
     eprintln!("Hello stderr!");
     println!("Hello stdout!");
-    trace!("Hello trace!");
-    debug!("Hello debug!");
-    info!("Hello info!");
-    warn!("Hello warn!");
-    error!("Hello error!");
 }
